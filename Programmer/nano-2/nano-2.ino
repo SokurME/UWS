93f3124fe6661bfd20c4493e69c48faa80b4646b
SokurME <sokurme@gmail.com>
2024-12-09 21:51:25 +0300

    в нано добавлена передача дистанции (проверить дистанцию!)

--- Programmer/nano-2/nano-2.ino
+++ Programmer/nano-2/nano-2.ino
@@ -1,16 +1,25 @@
 #include <TaskScheduler.h>
 
-byte cnt = 0; // счетчик для таймера экрана
+//byte cnt = 0; // счетчик для таймера экрана
+#define trigPin 3
+#define echoPin 4
+
+int counter;
+float duration;
+float distance;
+unsigned long time;
 
 // Создаем объекты 
 Scheduler userScheduler;   // планировщик
 
 void senddata();   //задаем прототип для отправки данных
 
-Task taskSenddata(TASK_MILLISECOND * 300, TASK_FOREVER, &senddata);   //указываем задание
+Task taskSenddata(TASK_MILLISECOND * 1000, TASK_FOREVER, &senddata);   //указываем задание
 
 void setup() {
   Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 
  //добавляем задания в обработчик
   userScheduler.addTask(taskSenddata);   
@@ -20,9 +29,47 @@ void setup() {
 void loop() {
   //запуск планировщика заданий
   userScheduler.execute();
-  cnt++;
+ // cnt++;
 }
 
 void senddata(){
- Serial.println(cnt);
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite( trigPin, LOW );
+  
+//  duration = pulseIn( echoPin, HIGH ); 
+
+  // Get Pulse duration with more accuracy than pulseIn()
+  duration = 0;
+  counter = 0;
+  while(--counter!=0 )
+  {
+    	if( PINB & 2 ) 
+    	{
+    	  time = micros();
+    	  break;
+    	}
+  }
+  counter = 0;
+  while( --counter!=0 )
+  {
+    	if( (PINB & 2)==0 ) 
+    	{
+    	  duration = micros()-time;
+    	  break;
+    	}
+  }
+
+  distance = ( duration/2 ) * 0.0344;
+  
+ /* Serial.print("Distance: ");
+
+  if     ( distance > 400 ) Serial.print("> 400");
+  else if( distance < 2 )   Serial.print("< 2");
+  else                      Serial.print( distance );
+
+  Serial.println( " cm" );*/
+ Serial.println(distance);
 }
